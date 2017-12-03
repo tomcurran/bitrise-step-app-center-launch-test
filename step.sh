5ac8ad68590282aa997fe2375470dc166139cb84
Tom Curran <tom@tomcurran.org>
2017-12-03 23:24:37 +0000

    added step with support for android

--- step.sh
+++ step.sh
@@ -1,25 +1,67 @@
 #!/bin/bash
-set -ex
 
-echo "This is the value specified for the input 'example_step_input': ${example_step_input}"
+# exit if a command fails
+set -e
 
 #
-# --- Export Environment Variables for other Steps:
-# You can export Environment Variables for other Steps with
-#  envman, which is automatically installed by `bitrise setup`.
-# A very simple example:
-envman add --key EXAMPLE_STEP_OUTPUT --value 'the value you want to share'
-# Envman can handle piped inputs, which is useful if the text you want to
-# share is complex and you don't want to deal with proper bash escaping:
-#  cat file_with_complex_input | envman add --KEY EXAMPLE_STEP_OUTPUT
-# You can find more usage examples on envman's GitHub page
-#  at: https://github.com/bitrise-io/envman
+# Required parameters
+if [ -z "${app_path}" ] ; then
+  echo " [!] Missing required input: app_path"
+  exit 1
+fi
+if [ ! -f "${app_path}" ] ; then
+  echo " [!] File doesn't exist at specified path: ${app_path}"
+  exit 1
+fi
+if [ -z "${app_center_app}" ] ; then
+  echo " [!] Missing required input: app_center_app"
+  exit 1
+fi
+if [ -z "${app_center_token}" ] ; then
+  echo " [!] Missing required input: app_center_token"
+  exit 1
+fi
+
+
+# ---------------------
+# --- Configs:
+
+echo " (i) Provided app path: ${app_path}"
+echo " (i) Provided app center app: ${app_center_app}"
+echo " (i) Provided app center token: ********"
+echo
+
+# ---------------------
+# --- Main
+
+npm config list
+
+npm install appcenter-cli@1.0.5 -g
+
+LAUNCH_TEST_DIR="${BITRISE_SOURCE_DIR}/app-center-launch-test"
+OUTPUT_PATH="${LAUNCH_TEST_DIR}/GeneratedTest"
+ARTIFACTS_DIR="${LAUNCH_TEST_DIR}/Artifacts"
+PLATFORM="Android"
+SOLUTION="${OUTPUT_PATH}/AppCenter.UITest.Android.sln"
+BUILD_DIR="${OUTPUT_PATH}/AppCenter.UITest.Android/bin/Release"
+MANIFEST_PATH="${ARTIFACTS_DIR}/manifest.json"
+
+# TODO remove - only for debug testing
+if [ -d "${OUTPUT_PATH}" ] ; then
+  rm -r "${OUTPUT_PATH}"
+fi
+
+appcenter test generate uitest --platform "${PLATFORM}" --output-path "${OUTPUT_PATH}"
+
+nuget restore -NonInteractive "${SOLUTION}"
+
+msbuild "${SOLUTION}" /p:Configuration=Release
+
+appcenter test prepare uitest --artifacts-dir "${ARTIFACTS_DIR}" --app-path "${app_path}" --build-dir "${BUILD_DIR}" --debug --quiet
+appcenter test run manifest --manifest-path "${MANIFEST_PATH}" --app-path "${app_path}" --app "${app_center_app}" --devices any_top_1_device --test-series launch-tests --locale en_US --debug --quiet --token "${app_center_token}"
 
 #
 # --- Exit codes:
 # The exit code of your Step is very important. If you return
 #  with a 0 exit code `bitrise` will register your Step as "successful".
 # Any non zero exit code will be registered as "failed" by `bitrise`.
-
-npm config list
-
